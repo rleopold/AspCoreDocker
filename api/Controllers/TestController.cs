8ee2bf4ab11eb5c0751a15f51954dfd57e400950
Richard J. Leopold <rjleopold@gmail.com>
2016-10-05 22:27:01 -0500

    fixed attribute routing

--- api/Controllers/TestController.cs
+++ api/Controllers/TestController.cs
@@ -1,20 +1,26 @@
 using System.Threading.Tasks;
+using Api.Core.Graph;
 using Microsoft.AspNetCore.Mvc;
 
-[RouteAttribute("api/[controller]")]
+
 public class TestController : Controller
 {
-    [HttpGet]
-    public IActionResult Get()
+    [HttpGet("api/people")]
+    public IActionResult GetAll()
     {
         return Json(new {Test = "GET", Success="true"});
     }
 
-    [HttpGetAttribute("{id}")]
+    [HttpGet("api/person/{id}")]
     public IActionResult Get(int id)
     {
         return Json(new {Test="GET_BY_ID", Success="true"});
     }
 
+    [HttpGet("api/person/getrandom")]
+    public IActionResult GetRandom()
+    {
+        return Json(new Person());
+    }
 
 }
